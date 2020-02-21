09bd5522aaf6b902e1109bcaf07ea21b2fe4dc7c
General Assembly <generalassembly@Generals-MacBook-Air-3.local>
2020-02-21 12:23:01 +1100

    ready for heroku deployment

--- db/db.js
+++ db/db.js
@@ -1,5 +1,11 @@
 const pg = require('pg')
-const pool = new pg.Pool({ database: 'parkdev1' }) // connect to the database
+let pool
+
+if (process.env.NODE_ENV === 'production') {
+  pool = new pg.Pool({ connectionString: process.env.DATABASE_URL }) // connect to the database on heroku
+} else {
+  pool = new pg.Pool({ database: 'parkdev1' }) // connect to the database
+}
 
 module.exports = {
   query: (sql, params) => {
